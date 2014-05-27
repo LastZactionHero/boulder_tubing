f990990a6a24a40cc47e2fa331186c80cdc8623f
Zach Dicklin <zdicklin@gmail.com>
2014-05-27 09:13:07 -0600

    Make it work

--- app/models/cached_data.rb
+++ app/models/cached_data.rb
@@ -1,57 +1,51 @@
 include ActionView::Helpers::SanitizeHelper
 
 class CachedData < ActiveRecord::Base
-  
+
   def self.get_instance
     instance = CachedData.first
     unless instance.present?
-      instance = CachedData.create( 
-        { 
-          :cfs => 0.0, 
+      instance = CachedData.create(
+        {
+          :cfs => 0.0,
           :current_temperature => 0.0,
           :current_condition => 'unknown',
           :today_high => 0.0,
           :today_condition => 'unknown',
           :cache_time => DateTime.now
         } )
-    end      
-    
+    end
+
     instance
   end
-  
+
   def expired?
     # 5 minute expiration
-    ( DateTime.now.to_i - self.cache_time.to_i ) > 5 * 60 || self.cfs <= 0.0            
+    ( DateTime.now.to_i - self.cache_time.to_i ) > 5 * 60 || self.cfs <= 0.0
   end
-  
+
   def cache_new_data
     a = Mechanize.new { |agent|
       agent.user_agent_alias = 'Mac Safari'
     }
-    
-    url = "http://www.dwr.state.co.us/SurfaceWater/data/detail_graph.aspx?ID=BOCOBOCO"
-    
+
+    url = "http://waterdata.usgs.gov/co/nwis/uv/?site_no=06730200"
+
     a.get( url ) do |page|
-      body = ActionView::Helpers::SanitizeHelper.strip_tags( page.body )
+      body = page.body
+      self.cfs = body.scan(/00060:[0-9]+:[0-9]+/).first.split(":")[1].to_f
       
-      start_parse_str = "Most Recent Value:"
-      end_parse_str = "cfs"
-      start_posn = body.index( start_parse_str ) + start_parse_str.length
-      end_posn = body.index( end_parse_str )
-      
-      self.cfs = body[ start_posn, end_posn - start_posn ].to_f
-        
       weather_client = Weatherman::Client.new( { :unit => "f" } )
       weather_response = weather_client.lookup_by_woeid 2367231
-      
+
       self.current_temperature = weather_response.condition['temp']
       self.current_condition = weather_response.condition['text']
       self.today_high = weather_response.forecasts[0]['high']
-      self.today_condition = weather_response.forecasts[0]['text']      
+      self.today_condition = weather_response.forecasts[0]['text']
     end
-    
+
     self.cache_time = DateTime.now
     self.save
   end
-  
+
 end
