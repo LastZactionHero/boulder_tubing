cef955fff70e9c49983124918fb3c394fbe4756e
Zach Dicklin <zdicklin@gmail.com>
2014-06-03 07:41:46 -0600

    Misc cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ include ActionView::Helpers::SanitizeHelper
 class HomeController < ApplicationController
 
   def index
-    @river_conditions = RiverConditions.new
-    @river_conditions.fetch!
+    @conditions = RiverConditions.new
+    @conditions.fetch!
   end
 
 end
